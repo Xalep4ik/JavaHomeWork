774a0571b73a1041d9f85411cf16b31cf15a94bf
Dexter <xalep4ik@gmail.com>
2021-10-14 09:28:22 +0300

    https://github.com/Xalep4ik/JavaHomeWork.git

---
+++ src/com/pb/kh/hw2/Calkulator.java
@@ -0,0 +1,50 @@
+package com.pb.kh.hw2;
+
+import java.util.Scanner;
+
+public class Calkulator {
+    static Scanner scan = new Scanner(System.in);
+        public static void main(String[] args) {
+
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int result = calc(a, b, getOperation());
+
+            System.out.println("Sum is " + result);
+        }
+            public static char getOperation(){
+            System.out.println("Choose operation: ");
+            char sign;
+            if(scan.hasNext()){
+                sign = scan.next().charAt(0);
+            } else {
+            scan.next();
+                sign = getOperation();
+            }
+            return sign;
+            }
+
+            public static int calc ( int a, int b, char sign){
+                int result;
+                switch (sign) {
+                    case '+':
+                        result = (a + b);
+                        break;
+                    case '-':
+                        result = (a - b);
+                        break;
+                    case '/':
+                        result = a / b;
+                        break;
+                    case '*':
+                        result = a * b;
+                        break;
+                    default:
+                      System.out.println("Error try again");
+                      result = calc(a, b, getOperation());
+
+                }
+                return result;
+            }
+
+}

ec625e0ea1459ad348ef522232075425002dbdb3
Dexter <xalep4ik@gmail.com>
2021-12-12 10:26:02 +0200

    Merge remote-tracking branch 'origin/master'

--- src/com/pb/kh/hw12/ContactList.java
+++ src/com/pb/kh/hw12/ContactList.java
@@ -1,23 +1,83 @@
 package com.pb.kh.hw12;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class ContactList {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
 
-        List<People> people = Arrays.asList(new People("Kat", LocalDate.of(1988, 12, 11), (98) - 888 - 88 - 88, "London"),
-                new People("Rik", LocalDate.of(1999, 2, 7), (96) - 767 - 67 - 67, "Berlin")
+        Scanner sc = new Scanner(System.in);
+
+        List<People> people = Arrays.asList(
+                new People("Kat", "88888888"),
+                new People("Rik", "123-234-22"),
+                new People("Morti", "7777777777")
         );
 
+        System.out.println("Ведите команду : " +
+                "new - для создания нового контакта " +
+                "delete - для удаления " +
+                "search - для поиска " +
+                "show n - вывод всех записей по имени " +
+                "show p - вывод всех записей по номеру " +
+                "renew - редактирование элемента " +
+                "exit - для выхода ");
+
+
+            String option = sc.nextLine();
+
+
+            switch (option) {
+                case "new":
+                    System.out.println("Введите данные контакта в формате : Имя, номер телефона, адресс, дата рождения (год,месяц,день) :");
+                    people.add(new People(sc.nextLine(), sc.nextLine()));
+                    System.out.println("Контакт добавлен !");
+                    System.out.println(LocalDateTime.now());
+                    break;
+                case "delete":
+                    System.out.println("Введите имя для удаления контакта : ");
+                    String nam = sc.nextLine();
+                    people.removeIf(people2 -> people2.name.equals(nam));
+                    System.out.println("Контакт удален ! " + LocalDateTime.now());
+                    break;
+                case "search":
+                    System.out.println("Введите имя для поиска :");
+                    System.out.println(SearchByName(people, sc.nextLine()));
+                    break;
+                case "show n":
+                    people.stream()
+                            .collect(Collectors.groupingBy(People::getName))
+                            .entrySet()
+                            .forEach(System.out::println);
+                    people.stream().close();
+                    break;
+                case "show p":
+                    people.stream()
+                            .collect(Collectors.groupingBy(People::getPhonenumber))
+                            .entrySet()
+                            .forEach(System.out::println);
+                    people.stream().close();
+                    break;
+                case "renew" :
+                    System.out.println ("Введите имя контакта");
+
+                    break;
+                case "exit":
+                    System.out.println("Завершение работы");
+                    break;
+            }
 
     }
 
     public static  List<People> SearchByName (List<People> people, String name){
         return people.stream().filter(people1 -> people1.name.equals(name)).collect(Collectors.toList());
+    }
+    public static boolean DeleteByName (List<People> people, String name){
+        return people.removeIf(peopl -> peopl.name.equals(name));
     }
 }
--- src/com/pb/kh/hw12/People.java
+++ src/com/pb/kh/hw12/People.java
@@ -3,22 +3,21 @@ package com.pb.kh.hw12;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
-import java.util.List;
 
 public class People {
 
         public String name;
-        private List<String> phonenumber;
-        public String adress;
-        private LocalDate date;
-        private LocalDateTime changed;
+        private String phonenumber;
+       /* public String adress;
+        private LocalDate date;*/
+        //private LocalDateTime changed;
 
-        public People(String name, List <String> phonenumber, String adress, LocalDate date, LocalDateTime changed) {
+        public People(String name,String phonenumber) {
             this.name = name;
-            this.date = date;
+            //this.date = date;
             this.phonenumber = phonenumber;
-            this.adress = adress;
-            this.changed = LocalDateTime.now();
+            //this.adress = adress;
+            //this.changed = LocalDateTime.now();
 
         }
 
@@ -30,39 +29,37 @@ public class People {
             this.name = name;
         }
 
-        public LocalDate getDate() {
+/*        public LocalDate getDate() {
             return date;
         }
 
         public void setDate(LocalDate date) {
             this.date = date;
-        }
+        }*/
 
-        public List<String> getPhonenumber() {
+        public String getPhonenumber() {
             return phonenumber;
         }
 
         public void setPhonenumber(String phonenumber) {
-            this.phonenumber = Collections.singletonList(phonenumber);
+            this.phonenumber =phonenumber;
         }
 
-        public String getAdress() {
+/*        public String getAdress() {
             return adress;
         }
 
         public void setAdress(String adress) {
             this.adress = adress;
-        }
+        }*/
 
-        @Override
-        public String toString() {
-            return "People{" +
-                    "name='" + name + '\'' +
-                    ", date=" + date +
-                    ", phonenumber=" + phonenumber +
-                    ", adress='" + adress + '\'' +
-                    '}';
-        }
+    @Override
+    public String toString() {
+        return "People{" +
+                "name='" + name + '\'' +
+                ", phonenumber='" + phonenumber + '\'' +
+                '}';
     }
+}
 
 
